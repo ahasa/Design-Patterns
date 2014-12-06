a089aec6a8bea2c0b30973bd017b8d55d35c17be
ahasa <ahmedsa@kth.se>
2014-12-06 13:05:58 +0100

    Editing Earth Object

--- creational/singleton/Earth.java
+++ creational/singleton/Earth.java
@@ -1,16 +1,3 @@
-/**
- * Singleton pattern is one of the simplest design patterns in Java. 
- * This type of design pattern comes under creational pattern as this
- *  pattern provides one of the best way to create an object.
- *  
- *  This pattern involves a single class which is responsible to creates own object while making sure that only single object get created. This class provides a way to access its only object which can be accessed directly without need to instantiate the object of the class.
- *  
- *  Implementation
- *  We're going to create a Earth class.
- *  Earth class have its constructor as private and have a static instance of itself.
- *  Earth class provides a static method to get its static instance to outside world.
- *  SingletonPatternDemo, our demo class will use SingleObjectInstance class to get a SingleObjectInstance object.
- */
 package singleton;
 
 import java.util.Date;
